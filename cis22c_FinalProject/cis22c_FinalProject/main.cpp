b6e11e376253970ec392b395872622127aad6a86
Mahsa <Mahsa@Mahsas-MacBook-Pro.local>
2014-11-08 01:24:42 -0800

    initial comment!

---
+++ cis22c_FinalProject/cis22c_FinalProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cis22c_FinalProject
+//
+//  Created by Mahsa Mirza on 11/8/14.
+//  Copyright (c) 2014 Mahsa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
